7dc2389600708553e9d5426bd95caece430cd4d0
ThomyALUN <tagudelo@unal.edu.co>
2025-03-05 22:32:19 -0500

    Add new enums and interface for message handling and data processing types

--- Front/src/utils/all.ts
+++ Front/src/utils/all.ts
@@ -2,7 +2,28 @@ export interface CsvRow {
   [key: string]: string | number;
 }
 
+export interface Message {
+  message: string;
+}
+
 export enum TaskTypes {
   REGRESSION = "regression",
   CLASSIFICATION = "classification",
+}
+
+export enum EncodingTypes {
+  ONE_HOT = "onehot",
+  FREQUENCY = "frequency",
+}
+
+export enum FillMethodTypes {
+  MEAN = "mean",
+  MEDIAN = "median",
+  MODE = "mode",
+  CONSTANT = "constant",
+}
+
+export enum ScalerTypes {
+  STANDARD = "standard",
+  MINMAX = "minmax",
 }
